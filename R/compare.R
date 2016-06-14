533e7cff1c73d7e7cc7851634f9533b82378102a
françois GUILLEM <francois.guillem@rte-france.com>
2016-06-14 16:43:35 +0200

    New function 'compare'

---
+++ R/compare.R
@@ -0,0 +1,84 @@
+#' Compare the surpluses of two simulations
+#'
+#' \code{compare} has been designed to compare two surpluses created with function
+#' \code{\link{surplus}} but it can be used to compare the values of two tables of
+#' class \code{antaresDataTable} that contain the same type of data.
+#'
+#' @param x
+#'   Table of class \code{antaresDataTable}
+#' @param y
+#'   Table of class \code{antaresDataTable}. It must contain the same type of data
+#'   than 'x': if 'x' contains areas, it must contain areas, ... Moreover it has
+#'   to have same time step and contain either synthetic or detailed results like
+#'   'x'.
+#' @param method
+#'   Method used two compare the two tables. \code{"diff"} compute the difference
+#'   between 'y' and 'x'. \code{"ratio"} computes the ratio between 'y' and 'x'.
+#'   Finally, \code{"rate"} computes the rate of change between 'y' and 'x' (
+#'   it is equal to the ratio between 'y' and 'x' minus one).
+#'
+#' @return
+#' a data.table of class \code{antaresDataTable}. It contains all shared rows and
+#' columns between 'x' and 'y'. The columns contains the statistic choosen:
+#' difference, ratio or rate of change.
+#'
+#' @examples
+#' \dontrun{
+#' # First simulation
+#' studyPath <- "path/to/study/"
+#'
+#' setSimulationPath(studyPath, 1)
+#' mydata1 <- readAntares("all", "all", synthesis = FALSE)
+#' surplus1 <- surplus(mydata1, groupByDistrict = TRUE)
+#'
+#' # Second simulation
+#' setSimulationPath(studyPath, 2)
+#' mydata2 <- readAntares("all", "all", synthesis = FALSE)
+#' surplus2 <- surplus(mydata2, groupByDistrict = TRUE)
+#'
+#' compare(surplus1, surplus2)
+#' }
+#'
+#' @export
+#'
+compare <- function(x, y, method=c("diff", "ratio", "rate")) {
+  method <- match.arg(method)
+
+  if (!is(x, "antaresDataTable")) stop("'x' must be a data.table of class 'antaresDataTable'")
+  if (!is(y, "antaresDataTable")) stop("'y' must be a data.table of class 'antaresDataTable'")
+
+  # Check that x and y are comparable
+  for (t in c("type", "timeStep", "synthesis")) {
+    if (attr(x, t) != attr(y, t)) {
+      stop("'x' and 'y' are not comparable. 'x' has ",t, " = ", attr(x, t),
+           " while 'y' has ", t, " = ", attr(y, t))
+    }
+  }
+
+  attrs <- attributes(x)
+
+  # Combine the two tables
+  sharedVars <- intersect(names(x), names(y))
+  sharedIdVars <- intersect(.idCols(x), .idCols(y))
+  valueVars <- setdiff(sharedVars, sharedIdVars)
+
+  x <- x[, sharedVars, with = FALSE]
+  y <- y[, sharedVars, with = FALSE]
+  setnames(y, valueVars, paste0(valueVars, "_y"))
+
+  res <- merge(x, y, by = sharedIdVars)
+
+  if (method == "diff") {
+    res[, c(valueVars) :=  res[, paste0(valueVars, "_y"), with = FALSE] - res[, valueVars, with = FALSE]]
+  } else if (method == "ratio") {
+    res[, c(valueVars) :=  res[, paste0(valueVars, "_y"), with = FALSE] / res[, valueVars, with = FALSE]]
+  } else if (method == "rate") {
+    res[, c(valueVars) :=  res[, paste0(valueVars, "_y"), with = FALSE] / res[, valueVars, with = FALSE] - 1]
+  } else stop("Invalid method")
+
+  res <- res[, sharedVars, with = FALSE]
+
+  .addClassAndAttributes(res, synthesis = attrs$synthesis, timeStep = attrs$timeStep,
+                         type = paste0(attrs$type, "Comparison"), opts = attrs$opts)
+
+}
---
+++ tests/testthat/test-compare.R
@@ -0,0 +1,64 @@
+context("Function compare")
+
+source("setup_test_case.R")
+opts <- setSimulationPath(studyPath)
+
+describe("compare", {
+
+  data1 <- readAntares(c("a", "b"), select = c("LOAD", "BALANCE"),
+                       timeStep = "annual", showProgress = FALSE)
+
+  it("returns an object with same structure than the input", {
+    data2 <- readAntares(c("a", "b"), select = c("LOAD", "BALANCE"),
+                         timeStep = "annual", showProgress = FALSE)
+    res <- compare(data1, data2)
+
+    expect_is(res, "antaresDataTable")
+    expect_identical(names(res), names(data1))
+  })
+
+  it("keeps only shared rows and columns", {
+    data2 <- readAntares(c("b", "c"), select = c("LOAD", "BALANCE", "MRG. PRICE"),
+                         timeStep = "annual", showProgress = FALSE)
+    res <- compare(data1, data2)
+    expect_identical(names(res),
+                     intersect(names(data1), names(data2)))
+    expect_identical(as.character(unique(res$area)),
+                     intersect(data1$area, data2$area))
+  })
+
+  it("stops if x and y have different types", {
+    data2 <- readAntares(links="all", select = c("FLOW LIN."),
+                         timeStep = "annual", showProgress = FALSE)
+    expect_error(compare(data1, data2), "type")
+  })
+
+  it("returns 0s if x = y", {
+    res <- compare(data1, data1)
+    expect_true(all(res$LOAD == 0 & res$BALANCE == 0))
+  })
+
+})
+
+mydata <- readAntares(c("a", "b"), select = c("LOAD"),
+                      timeStep = "annual", showProgress = FALSE)
+
+mydata2 <- copy(mydata)
+mydata2[, LOAD := LOAD * 1.2]
+
+test_that("Differences are correctly computed", {
+  res <- compare(mydata, mydata2, "diff")
+  expect_equal(res$LOAD, mydata$LOAD * 0.2)
+})
+
+test_that("Ratios are correctly computed", {
+  res <- compare(mydata, mydata2, "ratio")
+  expect_true(all(res$LOAD - 1.2 < 1e-10))
+})
+
+test_that("Evolutions are correctly computed", {
+  res <- compare(mydata, mydata2, "rate")
+  expect_true(all(res$LOAD - 0.2 < 1e-10))
+})
+
+
