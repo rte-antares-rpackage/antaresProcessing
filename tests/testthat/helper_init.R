3149bd104a75578d972de725f6f154612b68c6bb
zawam <jalal-edine.zawam@rte-france.com>
2018-05-02 16:53:35 +0200

    check if h5 is in tmp file for windows and debian

--- tests/testthat/helper_init.R
+++ tests/testthat/helper_init.R
@@ -6,7 +6,30 @@ path <- tempdir()
 sourcedir <- system.file("inst/testdata", package = "antaresRead")
 if(sourcedir == ""){ sourcedir <- system.file("testdata", package = "antaresRead")}
 
+check_if_h5_is_in_tmp<-function(h5filePath=NULL,path=NULL, stop=FALSE, printMessage=TRUE){
 
+  resH5NotInTmp<-!grepl("Temp", h5filePath, ignore.case = TRUE) & !grepl("tmp", h5filePath, ignore.case = TRUE)
+  if(resH5NotInTmp){
+    if(printMessage){
+      print(paste0("h5file : ", h5filePath))
+      print(paste0("path : ", path))
+    }
+  }else{
+    return(TRUE)
+  }
+
+  messageToPrint<-"h5file is not in temp folder"
+  if(stop & resH5NotInTmp){
+    stop(messageToPrint)
+  }
+  if(resH5NotInTmp){
+    if(printMessage){
+      message(messageToPrint)
+    }
+  }
+
+  return(FALSE)
+}
 
 Sys.unsetenv("R_TESTS")
 # Hack: For some unknown reason, this script is executed at some point of
@@ -34,12 +57,10 @@ if (sourcedir != "") {
       if(file.copy(from = h5file, to = path, overwrite = TRUE)){
         assign("h5file", file.path(path, nameH5File), envir = globalenv())
         #WE MUST assign h5file variable in the test environnement and not in the global environnement
-        if(!grepl("Temp", h5file, ignore.case = TRUE)){
+        if(!check_if_h5_is_in_tmp(h5file, path, printMessage = FALSE)){
           assign("h5file", file.path(path, nameH5File))
         }
-        if(!grepl("Temp", h5file, ignore.case = TRUE)){
-          print("h5file is not in temp folder")
-        }
+        check_if_h5_is_in_tmp(h5file, path)
       }
     }
 
@@ -53,11 +74,7 @@ if (sourcedir != "") {
       stop("h5file must not be null")
     }
 
-    if(!grepl("Temp", h5file, ignore.case = TRUE) & !grepl("tmp", h5file, ignore.case = TRUE) ){
-      print(paste0("h5file : ", h5file))
-      print(paste0("path : ", path))
-      stop("h5file is not in temp folder")
-    }
+    check_if_h5_is_in_tmp(h5file, path, stop = FALSE)
 
     silentf <- deprintize(showAliases)
     assign("silentf", silentf, envir = globalenv())
