1915ab348aabc462d561384cb5c1171127e459f4
Benoit Thieurmel <benoit.thieurmel@datastorm.fr>
2020-12-15 15:43:34 +0100

    fix addCongestionLink (hourly + mcYear + doc)

--- R/addCongestionLink.R
+++ R/addCongestionLink.R
@@ -1,18 +1,42 @@
 #Copyright © 2016 RTE Réseau de transport d’électricité
 
-#' Load factors of link
+#' Add congestion of link
 #'
-#' This function computes the load factor of link and add it to an
-#' \code{antaresData} object.
+#' This function computes 4 congestion variables of link and add it to an
+#' \code{antaresData} object. It's only be done on hourly data.
 #'
 #' @param x
 #'   Object of class \code{antaresData} created with function
 #'   \code{\link[antaresRead]{readAntares}}. It must contain the columns
 #'   \code{CONG. PROB +} and \code{CONG. PROB -}.
-#' @param timestep \code{character} timestep for aggregation.
+#' @param timestep \code{character} Desired time step for the result.
 #'
 #' @return
-#' \code{addLoadFactorLink}
+#' \code{addCongestionLink} modifies its input by adding four columns:
+#'
+#' \item{congestionFrequencyDirect}{
+#'
+#' \preformatted{congestionFrequencyDirect = round(sum((`CONG. PROB +` != 0)/.N), 2)}
+#'
+#' }
+#'
+#' \item{congestionFrequencyIndirect }{
+#'
+#' \preformatted{congestionFrequencyIndirect  = round(sum((`CONG. PROB -` != 0)/.N), 2)}
+#'
+#' }
+#'
+#' \item{congestionHoursDirect}{
+#'
+#' \preformatted{congestionHoursDirect  = sum(`CONG. PROB +` != 0)}
+#'
+#' }
+#'
+#' \item{congestionHoursIndirect }{
+#'
+#' \preformatted{congestionHoursIndirect =  sum(`CONG. PROB -` != 0)}
+#'
+#' }
 #'
 #' @examples
 #' \dontrun{
@@ -27,63 +51,54 @@
 #'
 #' @export
 #'
-addCongestionLink <- function(x, timestep = "daily") {
+addCongestionLink <- function(x,
+                              timeStep = c("daily", "weekly", "monthly", "annual")) {
 
-  if(!timestep %in% c("weekly", "daily", "monthly", "annual")){
-    stop("timestep must be weekly, daily, monthly or annual")
+  if(! "hourly" %in% attr(x, "timeStep")){
+    stop("'addCongestionLink' can be used only on 'hourly' data")
   }
 
-  if(timestep == "weekly"){
-    x$wd <- data.table::week(as.Date(x$time))
-    by = c("wd", "link")
-  }
-
-
-  if(timestep == "daily"){
-    by = c("link", "day", "month")
-  }
-  if(timestep == "monthly"){
-    by = c("link", "month")
-  }
-
-  if(timestep == "annual"){
-    by = c("link")
-  }
-
+  timeStep <- match.arg(timeStep)
 
   if (!is(x, "antaresData")) stop("'x' is not an 'antaresData' object")
 
-  #check when x is an antaresDataList
+  # check when x is an antaresDataList
   if (is(x, "antaresDataList")) {
     if(! c("links") %in% attr(x, "names")) stop("'x' does not contain link data")
-    if (!is.null(x$links)) x$links <- addCongestionLink(x$links)
+    if (!is.null(x$links)) x$links <- addCongestionLink(x$links, timeStep = timeStep)
     return(invisible(x))
   }
 
-  #check when x is an antaresData
+  # check when x is an antaresData
   if(! attr(x, "type") %in% c("links")  ) stop("'x' does not contain link data")
 
-  missingCols <- setdiff(c("CONG. PROB +", "CONG. PROB -"),
-                         names(x))
+  if(timeStep == "daily"){
+    by = c("link", "day", "month")
+  } else if(timeStep == "weekly"){
+    x$wd <- data.table::week(as.Date(x$time))
+    by = c("link", "wd")
+  } else if(timeStep == "monthly"){
+    by = c("link", "month")
+  } else if(timeStep == "annual"){
+    by = c("link")
+  }
+
+  if("mcYear" %in% names(x)){
+    by = c(by, "mcYear")
+  }
+  missingCols <- setdiff(c("CONG. PROB +", "CONG. PROB -"), names(x))
 
   if (length(missingCols) > 0) {
     stop("The following columns are needed but missing: ", paste(missingCols, collapse = ", "))
   }
 
-  # byVect <-  getIdCols(x)[getIdCols(x) %in% c("link")]
-  # byVect <- c(byVect, by)
+  x[, c("congestionFrequencyDirect", "congestionFrequencyIndirect", "congestionHoursDirect", "congestionHoursIndirect") := list(
+    round(sum((`CONG. PROB +` != 0)/.N), 2),
+    round(sum((`CONG. PROB -` != 0)/.N), 2),
+    sum(`CONG. PROB +` != 0),
+    sum(`CONG. PROB -` != 0)
+  ), by = by]
 
-  if(!all(by %in% names(x))){
-    bymiss <- by[!by %in% names(x)]
-    bymiss <- paste0(bymiss, collapse = ",")
-    stop(paste0("All columns specify in by must be in link data : ", bymiss))
-  }
-
-  x <- merge(x, unique(x[, .(congestionFrequencyDirect = round(sum((`CONG. PROB +` != 0)/.N), 2),
-        congestionFrequencyIndirect = round(sum((`CONG. PROB -` != 0)/.N), 2),
-        congestionHoursDirect = sum(`CONG. PROB +` != 0),
-        congestionHoursIndirect = sum(`CONG. PROB -` != 0)
-        ), by = by]), by = by)
-  if(timestep == "weekly")x$wd <- NULL
+  if(timeStep == "weekly") x$wd <- NULL
   invisible(x)
 }
